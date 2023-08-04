446c14e6003642d19855de26cccaccdea3728dd4
Gilang Fauzi <gilangfauzi023@gmail.com>
2023-08-04 10:59:34 +0700

    Manage UI Auth and Dashboard

---
+++ resources/views/layouts/admin/footer.blade.php
@@ -0,0 +1,6 @@
+<footer class="content-footer footer shadow-lg">
+    <div class="container-xxl d-flex flex-wrap justify-content-center py-2 flex-md-row flex-column">
+        © <script> document.write(new Date().getFullYear()); </script>, made with ❤️ by Gilang || 
+      <div id="times"></div>
+    </div>
+  </footer>
